65608c34b4ea100b9be777dbd96c05b96b6382d2
Robert F. Dickerson <rfdickerson@gmail.com>
2026-01-13 15:34:32 -0600

    add instances

---
+++ instances.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+
+#include "math.h"
+
+struct TreeInstance {
+    Vec3 position;
+    std::string variant;
+};
+
+struct PropInstance {
+    Vec3 position;
+    std::string type;
+};
